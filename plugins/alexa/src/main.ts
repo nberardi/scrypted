907994a644aba85d7e9beda37d4a12b9c0e29653
Nick Berardi <nberardi@gmail.com>
2026-09-07 18:52:55 -0400

    alexa: set SmartVision event payloadVersion to 1.0

--- plugins/alexa/src/main.ts
+++ plugins/alexa/src/main.ts
@@ -198,13 +198,17 @@ class AlexaPlugin extends ScryptedDeviceBase implements HttpRequestHandler, Mixi
 
         debug("event", eventDetails.eventInterface, eventDetails.property, eventSource.type);
 
+        const namespace = report?.event?.header?.namespace ?? "Alexa";
+        const payloadVersion = report?.event?.header?.payloadVersion
+            ?? (namespace === "Alexa.SmartVision.ObjectDetectionSensor" || namespace === "Alexa.DataController" ? "1.0" : "3");
+
         let data = {
             "event": {
                 "header": {
                     "messageId": createMessageId(),
-                    "namespace": report?.event?.header?.namespace ?? "Alexa",
+                    "namespace": namespace,
                     "name": report?.event?.header?.name ?? "ChangeReport",
-                    "payloadVersion": "3"
+                    "payloadVersion": payloadVersion
                 },
                 "endpoint": {
                     "endpointId": eventSource.id,
